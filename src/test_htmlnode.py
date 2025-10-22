4a0cf9442969e7ddf6f97b767757ae8a7ba89f5e
Jack TFB <jacktfbofficial@gmail.com>
2025-10-22 17:08:45 -0400

    LeafNode and ParentNode implementation

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -13,4 +13,26 @@ class TestHTMLNode(unittest.TestCase):
         self.assertNotEqual(node, node1)
         print(node.props_to_html())
         print("TEST SUCCESSFUL")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "This is important!")
+        self.assertEqual(node.to_html(), "<b>This is important!</b>")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
 
