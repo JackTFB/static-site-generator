19c8a0cb7b7495b24d79fd023a4def1422d9c64a
Jack TFB <jacktfbofficial@gmail.com>
2025-10-27 11:58:55 -0400

    Split Delimiter implementation

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,4 +1,5 @@
 import unittest
+from text_to_html import text_node_to_html_node
 
 from textnode import TextNode, TextType
 
@@ -18,6 +19,13 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
         print("TEST SUCCESSFUL")
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
 
 if __name__ == "__main__":
     unittest.main()
