fd0b8f9edf5191bd02c2b43acc842945991d77dd
Jack TFB <jacktfbofficial@gmail.com>
2025-11-13 16:10:10 -0500

    Project Reset

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from textnode import TextNode
-from textnode import TextType
-
-def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node.__repr__())
-
-main()
